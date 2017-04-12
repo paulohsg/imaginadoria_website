5680b6648c31df55a5d264aa9b03d69cee8c9b68
Paulo Henrique de Souza <paulohsg.es@gmail.com>
2017-04-11 22:07:11 -0300

    Contact area finished

--- js/script.js
+++ js/script.js
@@ -97,3 +97,79 @@ $(document).ready(function() {
             event.stopPropagation();
         });
     });
+
+
+
+$(window, document, undefined).ready(function() {
+
+  $('input').blur(function() {
+    var $this = $(this);
+    if ($this.val())
+      $this.addClass('used');
+    else
+      $this.removeClass('used');
+  });
+
+  var $ripples = $('.ripples');
+
+  $ripples.on('click.Ripples', function(e) {
+
+    var $this = $(this);
+    var $offset = $this.parent().offset();
+    var $circle = $this.find('.ripplesCircle');
+
+    var x = e.pageX - $offset.left;
+    var y = e.pageY - $offset.top;
+
+    $circle.css({
+      top: y + 'px',
+      left: x + 'px'
+    });
+
+    $this.addClass('is-active');
+
+  });
+
+  $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+  	$(this).removeClass('is-active');
+  });
+
+});
+
+
+$(document).ready(function() {
+
+	$('.button-form').on({
+	    'mouseenter':function(){
+	        $('.button-form-arrow').css('visibility', 'visible');
+        	$('.button-form-arrow').addClass('animated rotateIn');
+        	$('.button-form-arrow').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+
+		    		$('.button-form-arrow').removeClass('animated rotateIn');
+		    });
+
+	    },'mouseleave':function(){
+	        $('.button-form-arrow').addClass('animated rotateOut');
+
+	        $('.button-form-arrow').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+
+		    		$('.button-form-arrow').removeClass('animated rotateOut');
+		    		$('.button-form-arrow').css('visibility', 'hidden');
+		    });
+	    }
+	});
+
+
+	$('.icon-social-media').on({
+
+		'mouseenter':function(){
+			$(this).addClass('animated swing');
+		},
+		'mouseleave':function(){
+			$(this).removeClass('animated swing');
+		}
+
+
+	})
+
+});
